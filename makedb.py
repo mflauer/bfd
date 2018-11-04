46da319d4068f5861acc3b5322cea7f9a11c9fc5
Michelle Lauer <mflauer@mit.edu>
2018-11-03 23:10:41 -0400

    cleanup and baby support for selecting within columns

--- makedb.py
+++ makedb.py
@@ -1,49 +1,59 @@
 import sqlite3
 from sqlite3 import Error
 import pandas as pd
- 
+import numpy as np
+
 # inputs user expects to query over
 def make_db_for_user(original_csv, inputs: list):
     tableName = "test"
     # pull out only the columns relevant for their inputs
     original_df = pd.read_csv(original_csv)
 
-    colInputs = inputs # the inputs that are directly names of columns
+    colInputs = []
+    colInputs = list(inputs.keys()) # the inputs that are directly names of columns
     colFilteredDf = original_df[colInputs]
 
+    # do more granular filtering (more specific than just column level)
+    finalDf = colFilteredDf.copy()
+    for inpKey in inputs:
+        if inputs[inpKey] != None:
+            # we have requested a particular column value
+            finalDf = finalDf.loc[finalDf[inpKey] == inputs[inpKey]]
+
+    # generate column headers for the db
+    colHeaders = ""
+    numericCols = {}
+    for inp in inputs:
+        isNumeric = np.issubdtype(type(finalDf.iloc[0][inp]),np.number)
+        numericCols[inp] = isNumeric
+        if isNumeric:
+            data_type = "FLOAT"
+        else:
+            data_type = "TEXT"
+        colHeaders += ", " + inp + " " + data_type
 
     # go from our customized dataframe db file
     con = sqlite3.connect('{}.db'.format(tableName))
     with con:
         cur = con.cursor()
-        colHeaders = ""
-        for inp in inputs:
-            if "numpy.float" in str(type(colFilteredDf.iloc[0][inp])):
-                data_type = "FLOAT"
-            else:
-                data_type = "TEXT"
-            colHeaders += ", " + inp + " " + data_type
-
-        cur.execute("DROP TABLE {tablen}".format(tablen=tableName))
+        cur.execute("DROP TABLE IF EXISTS {tablen}".format(tablen=tableName))
         cur.execute("CREATE TABLE {tablen}(id INTEGER PRIMARY KEY{headers})".format(tablen=tableName, headers=colHeaders))
-        for index, row in colFilteredDf.iterrows():
+
+        for index, row in finalDf.iterrows():
             rowVal = str(index)
-            for inp in inputs:
-                current_val = row[inp]
-                if "float" in str(type(current_val)):
-                    if pd.isna(current_val):
-                        current_val = 0
-                    toAdd = str(current_val)
+            for colLabel in list(finalDf):
+                currentVal = row[colLabel]
+                if numericCols[colLabel]:
+                    toAdd = str(currentVal) if not pd.isna(currentVal) else str(0)
                 else:
-                    if pd.isna(current_val):
-                        current_val = ""
-                    toAdd = '"' + str(current_val) + '"'
+                    toAdd = '"' + str(currentVal) + '"' if not pd.isna(currentVal) else '""'
                 rowVal += "," + toAdd
-            print("INSERT INTO {tablen} VALUES({val})".format(tablen=tableName, val=rowVal))
+            #print("INSERT INTO {tablen} VALUES({val})".format(tablen=tableName, val=rowVal))
             cur.execute("INSERT INTO {tablen} VALUES({val})".format(tablen=tableName, val=rowVal))
 
     # return new db customized for them
 
+# used for testing purposes
 def read_file():
     cnx = sqlite3.connect("test.db")
     df = pd.read_sql_query("SELECT * FROM test LIMIT 5", cnx)
@@ -62,5 +72,5 @@ def create_connection(db_file):
  
 if __name__ == '__main__':
     #create_connection("C:\\sqlite\db\pythonsqlite.db")
-    make_db_for_user('movie_metadata.csv', ['director_name', 'duration'])
+    make_db_for_user('movie_metadata.csv', {'director_name': 'Peter Jackson', 'duration': None})
     read_file()
