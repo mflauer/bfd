e93aea12407f906f39f225d980f8340b32adb4e3
Jack Kelly <jgkelly@mit.edu>
2018-11-05 09:29:39 -0500

    More cleanup, added range and multiple selection db setup

--- makedb.py
+++ makedb.py
@@ -4,21 +4,28 @@ import pandas as pd
 import numpy as np
 
 # inputs user expects to query over
-def make_db_for_user(original_csv, inputs: list):
+def make_db_for_user(original_csv: str, inputs: dict):
     tableName = "test"
     # pull out only the columns relevant for their inputs
     original_df = pd.read_csv(original_csv)
 
-    colInputs = []
     colInputs = list(inputs.keys()) # the inputs that are directly names of columns
     colFilteredDf = original_df[colInputs]
 
     # do more granular filtering (more specific than just column level)
     finalDf = colFilteredDf.copy()
     for inpKey in inputs:
-        if inputs[inpKey] != None:
+        if inputs[inpKey] is None:
+            continue
+        elif type(inputs[inpKey]) == str:
             # we have requested a particular column value
             finalDf = finalDf.loc[finalDf[inpKey] == inputs[inpKey]]
+        elif type(inputs[inpKey]) == set:
+            # we have requested multiple values from column
+            finalDf = finalDf.loc[finalDf[inpKey].isin(inputs[inpKey])]
+        elif type(inputs[inpKey]) == list:
+            # We have requested range of values
+            finalDf = finalDf.loc[(finalDf[inpKey] > inputs[inpKey][0]) & (finalDf[inpKey] < inputs[inpKey][1])]
 
     # generate column headers for the db
     colHeaders = ""
@@ -32,7 +39,7 @@ def make_db_for_user(original_csv, inputs: list):
             data_type = "TEXT"
         colHeaders += ", " + inp + " " + data_type
 
-    # go from our customized dataframe db file
+    # go from our customized dataframe to db file
     con = sqlite3.connect('{}.db'.format(tableName))
     with con:
         cur = con.cursor()
@@ -48,7 +55,6 @@ def make_db_for_user(original_csv, inputs: list):
                 else:
                     toAdd = '"' + str(currentVal) + '"' if not pd.isna(currentVal) else '""'
                 rowVal += "," + toAdd
-            #print("INSERT INTO {tablen} VALUES({val})".format(tablen=tableName, val=rowVal))
             cur.execute("INSERT INTO {tablen} VALUES({val})".format(tablen=tableName, val=rowVal))
 
     # return new db customized for them
@@ -56,21 +62,12 @@ def make_db_for_user(original_csv, inputs: list):
 # used for testing purposes
 def read_file():
     cnx = sqlite3.connect("test.db")
-    df = pd.read_sql_query("SELECT * FROM test LIMIT 5", cnx)
+    df = pd.read_sql_query("SELECT movie_title FROM test LIMIT 5", cnx)
     for ind, row in df.iterrows():
         print(row)
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        conn.close()
  
 if __name__ == '__main__':
-    #create_connection("C:\\sqlite\db\pythonsqlite.db")
-    make_db_for_user('movie_metadata.csv', {'director_name': 'Peter Jackson', 'duration': None})
+    make_db_for_user('movie_metadata.csv', {'director_name': {'Christopher Nolan', 'James Cameron'}, 'duration': [100, 120], 'movie_title': None})
     read_file()
+
