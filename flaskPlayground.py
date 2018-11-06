20bb06ace8d7bb1a90ebda03a17a6da56cbbc75e
Michelle Lauer <mflauer@mit.edu>
2018-11-05 19:16:54 -0500

    uplooaaaadddd

--- flaskPlayground.py
+++ flaskPlayground.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
@@ -10,11 +10,12 @@ def output():
 @app.route('/receiver', methods = ['POST'])
 def worker():
     # read json + reply
-    print(request.get_json())
+    # print(request.get_json())
     # with open('testFile.txt', "wb") as file:
     #     file.write(request.form)
-    return "Hello"
-
+    f = request.files['file']
+    print(f.read())
+    return redirect("http://www.example.com")
 
 if __name__ == "__main__":
     app.run("0.0.0.0", "5010")
