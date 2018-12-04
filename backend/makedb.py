3dccc11cd8f71ab8fed8779d283cef33acc1248f
Jack Kelly <jgkelly@mit.edu>
2018-12-04 17:03:32 -0500

    Put db in data_files

--- backend/makedb.py
+++ backend/makedb.py
@@ -1,7 +1,7 @@
 import sqlite3
-from sqlite3 import Error
 import pandas as pd
 import numpy as np
+import os
 
 
 # inputs user expects to query over
@@ -40,9 +40,10 @@ def make_db_for_user(original_csv: str, inputs: dict, table_name: str = "test"):
 
     # go from our customized dataframe to db file
 
-    con = sqlite3.connect('{}.db'.format(table_name))
-    with con:
-        cur = con.cursor()
+    directory = os.path.join(get_base_directory(), "data_files/")
+    conn = sqlite3.connect(directory + table_name + ".db")
+    with conn:
+        cur = conn.cursor()
         cur.execute("DROP TABLE IF EXISTS {tablen}".format(tablen=table_name))
         cur.execute("CREATE TABLE {tablen}(id INTEGER PRIMARY KEY{headers})".format(tablen=table_name, headers=colHeaders))
 
@@ -60,21 +61,34 @@ def make_db_for_user(original_csv: str, inputs: dict, table_name: str = "test"):
     # return new db customized for them
     return colInputs
 
-def query_db(sql_query, tableName):
-    conn = sqlite3.connect(tableName + ".db")
+
+def query_db(sql_query, table_name):
+    directory = os.path.join(get_base_directory(), "data_files/")
+    conn = sqlite3.connect(directory + table_name + ".db")
     cur = conn.cursor()
     results = cur.execute(sql_query).fetchall()
     return results
 
+
 # used for testing purposes
 def read_file(table_name):
-    cnx = sqlite3.connect("{}.db".format(table_name))
-    df = pd.read_sql_query("SELECT * FROM {} LIMIT 10".format(table_name), cnx)
+    directory = os.path.join(get_base_directory(), "data_files/")
+    conn = sqlite3.connect(directory + table_name + ".db")
+    df = pd.read_sql_query("SELECT * FROM {} LIMIT 10".format(table_name), conn)
     for ind, row in df.iterrows():
         print(row)
+
+
+def get_base_directory():
+    current_directory = os.getcwd()
+    path = ("", current_directory)
+    while path[1] != "bfd":
+        path = os.path.split(current_directory)
+        current_directory = path[0]
+    return os.path.join(path[0], path[1])
 
  
 if __name__ == '__main__':
     # make_db_for_user('movie_metadata.csv', {'director_name': {'Christopher Nolan', 'James Cameron'}, 'duration': [100, 120], 'movie_title': None})
-    print(query_db("SELECT director_name FROM BFD WHERE movie_title = 'Avatar\xa0' ;", "BFD"))
+    print(query_db("SELECT director_name FROM BFD limit 50;", "BFD"))
 
